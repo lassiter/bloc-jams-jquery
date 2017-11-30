da85352e670325266a845121830b4520bd7587a1
Lassiter Gregg <lassitergregg@gmail.com>
2017-11-29 20:05:04 -0600

    added prettyTime

--- scripts/helper.js
+++ scripts/helper.js
@@ -3,7 +3,9 @@ class Helper {
   playPauseAndUpdate(song){
     const duration = player.getDuration();
     player.playPause(song);
-    $('#time-control .total-time').text( duration );
+    $('#time-control .total-time').text(
+      player.prettyTime( duration )
+    );
   }
 }
 
